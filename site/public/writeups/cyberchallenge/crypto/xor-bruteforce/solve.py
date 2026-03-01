be0e7aaf1b581316f04f380165a6fa9976ee695d
Giordano Fornari <gfornari.casa@gmail.com>
2026-03-01 19:59:00 +0100

    refactoring

--- site/public/writeups/cyberchallenge/crypto/xor-bruteforce/solve.py
+++
@@ -1,6 +0,0 @@
-from chepy import Chepy
-ciphertext = "104e137f425954137f74107f525511457f5468134d7f146c4c"
-
-plaintext = Chepy(ciphertext).xor_bruteforce()
-#plaintext = "flag{" + str(plaintext) + "}"
-print(plaintext)
