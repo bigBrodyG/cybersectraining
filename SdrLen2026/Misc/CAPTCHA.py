dd4da19750bee892665fd3f0dce7f69430ee5a05
Giordano Fornari <gfornari.casa@gmail.com>
2026-03-01 18:51:42 +0100

    site built

---
+++ SdrLen2026/Misc/CAPTCHA.py
@@ -0,0 +1,10 @@
+"""
+Emoji CAPTCHA
+0 0 500
+misc
+uNickz
+
+CAPTCHAs were invented to keep robots out and let humans in. We decided to reverse the rules.
+
+This is a remote challenge, you can connect to the service with: nc emoji.challs.srdnlen.it 1717
+"""
